a782410b5ee81a47bf82fe35dfbe6cceac53e21e
Aidan Woolley <aw813@cam.ac.uk>
2021-05-10 07:40:57 +0100

    Configure congested network

--- src/rust/catnip_mininet/catnip_test_topo.py
+++ src/rust/catnip_mininet/catnip_test_topo.py
@@ -9,7 +9,7 @@ topology enables one to pass in '--topo=mytopo' from the command line.
 """
 
 from mininet.topo import Topo
-from mininet.link import TCLink
+from mininet.link import TCLink, TCULink
 
 class CatnipTopo(Topo):
     "Simple topology example."
@@ -24,11 +24,13 @@ class CatnipTopo(Topo):
 
         bob = self.addHost("bob", ip="10.0.0.2/24", mac="12:23:45:67:89:B0")
     
-        switch = self.addSwitch("s1")
+        s_alice = self.addSwitch("s1")
+        s_bob = self.addSwitch("s2")
 
         # Add links
-        self.addLink(alice, switch, cls=TCLink, bw=10, delay="50ms")
-        self.addLink(switch, bob, cls=TCLink, bw=10, delay="50ms")
+        self.addLink(alice, s_alice, cls=TCULink, bw=10, delay="50ms")
+        self.addLink(s_alice, s_bob, cls=TCULink, bw=1, max_queue_size=10, delay="50ms")
+        self.addLink(s_bob, bob, cls=TCULink, bw=10, delay="50ms")
 
 
 topos = {'catniptopo': lambda: CatnipTopo()}
